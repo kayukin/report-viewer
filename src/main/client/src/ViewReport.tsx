ef5acafdd75dd9bdf6d3de2bbaa4576b0ebfd020
Konstantin Kayukin <t_kkayukin@admarketplace.com>
2024-07-11 23:28:47 +0200

    Add loader

--- src/main/client/src/ViewReport.tsx
+++ src/main/client/src/ViewReport.tsx
@@ -2,7 +2,7 @@ import './ViewReport.css'
 import {Link, useSearchParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import {Env} from "./Env.ts";
-import {AppBar, IconButton, Toolbar, Typography} from "@mui/material";
+import {AppBar, Backdrop, CircularProgress, IconButton, Toolbar, Typography} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Home";
 import {encodeQuery} from "./Utils.ts";
 
@@ -10,13 +10,25 @@ export const ViewReport = () => {
     const [searchParams] = useSearchParams();
     const key = searchParams.get("key")!;
     const [link, setLink] = useState('');
+    const [loading, setLoading] = useState(true);
+
     useEffect(() => {
         fetch(`${Env.API_BASE_URL}/reports/view?` + encodeQuery({key}))
             .then(value => value.text())
-            .then(value => setLink(value));
-    }, [key])
+            .then(value => {
+                setLink(value);
+                setLoading(false);
+            });
+    }, [key]);
+
     return (
         <>
+            <Backdrop
+                sx={{color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1}}
+                open={loading}
+            >
+                <CircularProgress color="inherit"/>
+            </Backdrop>
             <AppBar position="fixed">
                 <Toolbar>
                     <IconButton
